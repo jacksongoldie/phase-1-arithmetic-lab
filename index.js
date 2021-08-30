641d74d3fd1bd8e4decd6c2bb1dcfcb0d63ceb41
Goldie <goldiejackson25@yahoo.com>
2021-08-30 16:24:05 -0500

    tests passing

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 1, num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 18, num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
